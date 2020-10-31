1cc0a765776365171de442fd93b1c656f6037f14
Avinash Vakil <m@avinashvakil.com>
2020-10-30 23:15:26 -0700

    Fix header for older version of SKSE

---
+++ include/SKSE_PCH.h
@@ -0,0 +1,9 @@
+// This header is force included into SKSE's build because for some reason they need to import <string>
+
+#ifndef SKYRIMOUTFITSYSTEMSE_SKSE_PCH_H
+#define SKYRIMOUTFITSYSTEMSE_SKSE_PCH_H
+
+#include <string>
+#include "../dependencies/skse64/src/common/IPrefix.h"
+
+#endif //SKYRIMOUTFITSYSTEMSE_SKSE_PCH_H
