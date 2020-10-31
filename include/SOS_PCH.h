78562e270379d2c8fa1a1ba3a366ba7b0853d9c6
MetricExpansion <>
2020-10-30 22:26:10 -0700

    Added CMakeLists files. Basic functionality.

---
+++ include/SOS_PCH.h
@@ -0,0 +1,11 @@
+//
+// Created by m on 10/30/2020.
+//
+
+#ifndef SKYRIMOUTFITSYSTEMSE_SOS_PCH_H
+#define SKYRIMOUTFITSYSTEMSE_SOS_PCH_H
+
+#include "SKSE/Impl/PCH.h"
+#include "common/IPrefix.h"
+
+#endif //SKYRIMOUTFITSYSTEMSE_SOS_PCH_H
