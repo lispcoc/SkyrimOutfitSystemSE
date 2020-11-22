b2e76681b25456bc2cd3c614948424b9c0b3b661
Avinash Vakil <m@avinashvakil.com>
2020-11-22 12:31:27 -0800

    Cutting down the generated header.

--- src/addrlib/main.cpp
+++ src/addrlib/main.cpp
@@ -55,7 +55,7 @@ public:
     }
 };
 
-void check_file(std::vector<std::tuple<std::string, std::string>>& failing, const std::filesystem::path& path, const std::map<unsigned long long, unsigned long long>& addrToId) {
+void check_file(std::map<unsigned long long, unsigned long long>& usedAddrToId, std::vector<std::tuple<std::string, std::string>>& failing, const std::filesystem::path& path, const std::map<unsigned long long, unsigned long long>& addrToId) {
     std::ifstream file(path);
     std::string line;
     std::regex hex_num_re("0[xX][0-9a-fA-F]{8}", std::regex_constants::ECMAScript);
@@ -64,20 +64,24 @@ void check_file(std::vector<std::tuple<std::string, std::string>>& failing, cons
         for (std::sregex_iterator match = words_begin; match != std::sregex_iterator(); ++match) {
 //            std::cout << match->str() << std::endl;
             uintptr_t match_int = std::stoull(match->str(), 0, 16);
-            if (addrToId.find(match_int) == addrToId.end()) {
+            auto findMapping = addrToId.find(match_int);
+            if (findMapping == addrToId.end()) {
                 failing.emplace_back(path.string(), match->str());
+            } else {
+                usedAddrToId.emplace(*findMapping);
             }
         }
     }}
 
-void check_source_files(const char* path, const std::map<unsigned long long, unsigned long long>& addrToId) {
+std::map<unsigned long long, unsigned long long> check_source_files(const char* path, const std::map<unsigned long long, unsigned long long>& addrToId) {
     using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
     std::vector<std::tuple<std::string, std::string>> failing;
+    std::map<unsigned long long, unsigned long long> usedAddrToId;
     try {
 //        std::cout << "Checking source files in " << path << std::endl;
         for (const auto& dirEntry : recursive_directory_iterator(path)) {
 //             std::cout << dirEntry.path() << std::endl;
-             check_file(failing, dirEntry.path(), addrToId);
+             check_file(usedAddrToId, failing, dirEntry.path(), addrToId);
         }
     } catch (std::filesystem::filesystem_error& e) {
         std::cerr << "Could not find " << path << std::endl;
@@ -88,9 +92,11 @@ void check_source_files(const char* path, const std::map<unsigned long long, uns
             std::cerr << "MISSING OFFSET " << std::get<1>(result) << " IN " << std::get<0>(result) << std::endl;
         }
     }
+    return usedAddrToId;
 }
 
 int main(int argc, char *argv[]) {
+    // Read CLI arguments and open the offset database.
     if (argc != 5) {
         std::cerr << "You need to pass the following: version address_library_location header_write_location skse_location" << std::endl;
         return 1;
@@ -113,38 +119,46 @@ int main(int argc, char *argv[]) {
             std::cout << "Address Library loaded database." << std::endl;
         }
     }
-    std::ofstream output(std::string(argv[3]) + "\\offsets_" + argv[1] + ".h");
+
+    // Check all source files for which addresses are actually used.
+    // Also dump the database to txt.
+    std::cout << "Writing database dump." << std::endl;
     std::ofstream output_txt(std::string(argv[3]) + "\\offsets_" + argv[1] + ".txt");
     auto db = vdb.GetOffsetMap();
-//    output << "#pragma once\n"
-//              "#include <skse64_common/eternal.hpp>"
-//              "\n"
-//              "namespace Offsets {\n"
-//              "\t// Generated code from addrheader\n"
-//              "\tconstexpr const auto addrMap = mapbox::eternal::map<uintptr_t, uintptr_t>({" << "\n";
     char buffer[1000];
-    auto last_iter = --db.cend();
     std::map<unsigned long long, unsigned long long> reverse_map;
     for (auto record = db.cbegin(); record != db.cend(); ++record) {
-//        sprintf(buffer, "\t\t{ %lli, %lli }", record->second, record->first);
-//        if (record != last_iter) {
-//            output << buffer << "," << "\n";
-//        } else {
-//            output << buffer << "\n";
-//        }
         sprintf(buffer, "%lli %lli\n", record->second, record->first);
         output_txt << buffer;
         reverse_map[record->second] = record->first;
     }
-//    output << "\t});\n"
-//              "}\n" << std::endl;
+    output_txt << std::endl;
+    output_txt.close();
+    std::cout << "Scanning SKSE source code." << std::endl;
+    auto usedAddrToId = check_source_files(argv[4], reverse_map);
+    // Write header
+    std::cout << "Writing address map header." << std::endl;
+    std::ofstream output(std::string(argv[3]) + "\\offsets_" + argv[1] + ".h");
+    output << "#pragma once\n"
+              "#include <skse64_common/eternal.hpp>"
+              "\n"
+              "namespace Offsets {\n"
+              "\t// Generated code from addrheader\n"
+              "\tconstexpr const auto addrMap = mapbox::eternal::map<uintptr_t, uintptr_t>({" << "\n";
+    auto last_iter = --usedAddrToId.cend();
+    for (auto record = usedAddrToId.cbegin(); record != usedAddrToId.cend(); ++record) {
+        sprintf(buffer, "\t\t{ %lli, %lli }", record->first, record->second);
+        if (record != last_iter) {
+            output << buffer << "," << "\n";
+        } else {
+            output << buffer << "\n";
+        }
+    }
+    output << "\t});\n"
+              "}\n" << std::endl;
     output << "#define BUILT_AGAINST_SKYRIM_MAJOR " << major << "\n";
     output << "#define BUILT_AGAINST_SKYRIM_MINOR " << minor << "\n";
     output << "#define BUILT_AGAINST_SKYRIM_REVISION " << revision << "\n";
     output.close();
-    output_txt << std::endl;
-    output_txt.close();
-    std::cout << "Wrote header." << std::endl;
-    check_source_files(argv[4], reverse_map);
-    std::cout << "Done." << std::endl;
+    std::cout << "Address map complete." << std::endl;
 }
